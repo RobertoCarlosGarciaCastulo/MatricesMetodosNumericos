948b39d55609575ccde8602839492084ea3edeb3
RobertoCarlosGarciaCastulo <52472433+RobertoCarlosGarciaCastulo@users.noreply.github.com>
2019-08-14 11:25:24 -0500

    Add files via upload

--- Test11.py
+++ Test11.py
@@ -1,7 +1,9 @@
-from Lineal import lagrange
+from Interpolacion import lagrange
 import pylab as pl
 x=[2,4,5]
 y=[5,6,3]
+p=lagrange(x,y)
+print(p)
 u=pl.arange(2,5.1,0.1)
 v=lagrange(x,y,list(u))
 pl.plot(u,v)
---
+++ Test3.py
@@ -0,0 +1,9 @@
+from numpy import*
+from Lineal import*
+a=array([[4,2,5],[2,5,8],[2,4,3]],float)
+#print (a)
+print ("")
+b=array([[18],[27.3],[16.2]],float)
+#print (b)
+print ("")
+print(gauss1(a,b))
